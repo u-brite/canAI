c7bd43a2e4ee33e2708a029aa3709ba546c3435c
jjjk123 <jj.kubica@student.uw.edu.pl>
2022-08-05 19:29:58 +0200

    upd

--- app.py
+++ app.py
@@ -3,38 +3,46 @@ import streamlit as st
 import numpy as np
 import matplotlib.pyplot as plt
 
+st.sidebar.markdown('U-BRITE Hackin\' Omics 2022!')
+st.title('canAI')
+st.markdown('Comparing feature extraction methods for biomarker discovery in a pan-cancer study')
+
 @st.cache
 def load_file(file_name):
     return pd.read_csv(file_name)
 
 df = load_file('Prostate.csv')
 
-st.sidebar.markdown('U-BRITE Hackin\' Omics 2022!')
-st.title('canAI')
-st.markdown('Comparing feature extraction methods for biomarker discovery in a pan-cancer study')
+column = st.selectbox('Select column to display', df.columns, index=0)
 
-col1, col2 = st.columns(2)
+unique_values = df[column].unique()
 
-with col1:
-    treatment_type = df['treatment_type'].unique().tolist()
-    column = st.selectbox('Treatment Type', treatment_type, index=0)
+c1 = st.selectbox('Class A', unique_values, index=0)
+c2 = st.selectbox('Class B', unique_values, index=0)
 
-with col2:
-    age_at_index = df['age_at_index'].unique().tolist()
-    column = st.selectbox('Age', age_at_index, index=0)
+# col1, col2 = st.columns(2)
 
-st.header('Data Summary')
+# with col1:
+#     treatment_type = df['treatment_type'].unique().tolist()
+#     column = st.selectbox('Treatment Type', treatment_type, index=0)
 
-st.markdown('Number of Cases:', len(df.index))
+# with col2:
+#     age_at_index = df['age_at_index'].unique().tolist()
+#     column = st.selectbox('Age', age_at_index, index=0)
 
-if st.checkbox('Show Raw Data'):
-    st.subheader('Raw Data')
-    st.write(df)
+# st.header('Data Summary')
 
-st.set_option('deprecation.showPyplotGlobalUse', False)
-df.hist()
-st.pyplot()
+# st.write('Number of Cases:', len(df.index))
 
-agree = st.button('Click to see treatment types')
-if agree:
- st.bar_chart(df['treatment_type'])
+# if st.checkbox('Show Raw Data'):
+#     st.subheader('Raw Data')
+#     st.write(df)
+
+# st.set_option('deprecation.showPyplotGlobalUse', False)
+# df.hist()
+# st.pyplot()
+
+st.bar_chart(df[c1])
+# agree = st.button('Click to see treatment types')
+# if agree:
+#  st.bar_chart(df[c1])
